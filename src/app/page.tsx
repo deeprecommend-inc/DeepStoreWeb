8d785ade3f5201ce4ca70dac52920e38e63b5e7f
jin-sugimoto-for-business <tatoonashi0203@icloud.com>
2024-02-11 21:48:47 +0900

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
 "use client";
 import Image from "next/image";
 import * as React from "react";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 import { Calendar } from "../components/Calendar";
-
-export function BasicDateCalendar() {
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar />
-    </LocalizationProvider>
-  );
-}
 
 export default function Home() {
   return (
