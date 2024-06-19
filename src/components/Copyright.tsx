81f795ad2c20339f7b5ea03dc4dc630bd721e3a1
jin-sugimoto-for-business <tatoonashi0203@icloud.com>
2024-06-19 09:12:20 +0900

    main

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -3,19 +3,19 @@ import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 
 export const Copyright = (props: any) => {
-	return (
-		<Typography
-			variant="body2"
-			color="text.secondary"
-			align="center"
-			{...props}
-		>
-			{"Copyright © "}
-			<Link color="inherit" href="https://kaitori-daikichi-hatsushiba.jp/">
-        kaitori-daikichi.jp
-			</Link>{" "}
-			{new Date().getFullYear()}
-			{"."}
-		</Typography>
-	);
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://kaitori-daikichi-hatsushiba.jp/">
+        kaitori-daikichi-hatsushiba.jp
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 };
